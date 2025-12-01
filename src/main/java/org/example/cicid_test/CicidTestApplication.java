8a20f41dbd6fbd0c8547ce22e9a718cfaf54bc92
yeejini <ks990528@naver.com>
2025-12-01 09:26:30 +0900

    first commit

---
+++ src/main/java/org/example/cicid_test/CicidTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.cicid_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicidTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicidTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/cicid_test/CicidTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cicid_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicidTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
