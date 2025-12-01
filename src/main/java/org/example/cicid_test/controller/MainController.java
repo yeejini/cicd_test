aa9284474c28015f7da6da7e27908b8334321909
yeejini <ks990528@naver.com>
2025-12-01 10:49:46 +0900

    controller

---
+++ src/main/java/org/example/cicid_test/controller/MainController.java
@@ -0,0 +1,14 @@
+package org.example.cicid_test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MainController {
+    @GetMapping
+    public  String main(){
+        return "This is a manual deployment test server";
+    }
+}
